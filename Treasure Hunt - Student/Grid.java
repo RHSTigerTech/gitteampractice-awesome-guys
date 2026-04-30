1c8dce64a816be3b73cfd25c36cdb94f3a0f1e9e
Julie <malinanguyen23@outlook.com>
2026-04-30 13:06:29 -0700

    the throngler is here

--- Treasure Hunt - Student/Grid.java
+++ Treasure Hunt - Student/Grid.java
@@ -29,6 +29,7 @@ public class Grid {
 
         //Add Foes that you would need to battle or interact with...
         kingdom[1][2].addFoe(new Foe("Guart", 7));
+        kingdom[1][1].addFoe(new Foe("The Throngler", 200));
 
 
     }
